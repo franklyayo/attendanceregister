1b99802314f4518c68705df6391f2c07c5b1d3c4
franklyayo <149245900+franklyayo@users.noreply.github.com>
2026-04-01 01:41:47 +0100

    Refactor MySQL connection to use env variables
    Updated MySQL connection configuration to use environment variables and added SSL options for Azure compatibility.

--- utils/index.js
+++ utils/index.js
@@ -2,12 +2,14 @@ import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
 
 const connection = await mysql.createPool({
-//  host: process.env.NEXT_PUBLIC_HOST,
-host: '109.70.148.55',
-  user: "quodeshi_attend",
-  database: 'quodeshi_attendanceapp',
-  password: 'Attendance123#',
-  port: '3306'
+  host: process.env.NEXT_PUBLIC_HOST,
+  user: process.env.NEXT_PUBLIC_USER,
+  database: process.env.NEXT_PUBLIC_DATABASE,
+  password: process.env.NEXT_DB_PASSWORD,
+  port: 3306,
+  ssl: {
+    rejectUnauthorized: false   // ← Required for Azure MySQL Flexible Server
+  }
 });
 
 export const db = drizzle(connection);
